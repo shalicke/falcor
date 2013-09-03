f88fffc3bc6cb46b3214f23069bbbaaeb921caca
Sam Halicke <sam@halicke.com>
2013-09-03 13:06:17 -0500

    updating deps

--- project.clj
+++ project.clj
@@ -6,15 +6,18 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojars.samhalicke/compojure "1.2.1-SNAPSHOT"]
                  [ring/ring-jetty-adapter "1.2.0"]
-                 [ring/ring-devel "1.2.0"]
                  [ring-basic-authentication "1.0.2"]
                  [environ "0.4.0"]
-                 [org.clojars.samhalicke/hiccup-bootstrap "0.2.5"]
+                 [org.clojars.samhalicke/hiccup-bootstrap "0.2.6"]
                  [com.cemerick/drawbridge "0.0.6"]
                  [hiccup "1.0.4"]
                  [clj-time "0.6.0"]
                  [com.novemberain/monger "1.6.0"]
                  ]
+  :dev {
+        :dependencies [
+                       [ring/ring-devel "1.2.0"]
+                       ]}
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.2.1"]]
   :hooks [environ.leiningen.hooks]

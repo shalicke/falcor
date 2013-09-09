4012e63e6bb0e220c402b093b4cf79b9623595dd
Sam Halicke <sam@halicke.com>
2013-09-08 23:43:44 -0500

    working posts for the most part

--- src/falcor/views.clj
+++ src/falcor/views.clj
@@ -5,6 +5,7 @@
             [hiccup.element :refer :all]
             [hiccup.bootstrap.element :refer :all]
             [hiccup.bootstrap.page :refer :all]
+            [falcor.blog :as blog]
             [falcor.content :as content :refer :all]))
 
 (defn template-base
@@ -28,36 +29,37 @@
      (include-css "/css/falcor.css")
      "<!--[if lt IE 9]><script src=\"/js/html5shiv.js\"></script><script src=\"/js/respond.min.js\"></script><![endif]-->"]
     [:body
-     [:div.container
-      [:nav.navbar.navbar-defaultnavbar-static-top
-       {:role "navigation"}
-       [:div.navbar-header
-        [:button.navbar-toggle
-         {:type "button" :data-toggle "collapse" :data-target "navbar-home-collapse"}
-         [:span.sr-only "Toggle navigation"]
-         [:span.icon-bar]
-         [:span.icon-bar]
-         [:span.icon-bar]
+     [:div#wrap
+      [:div.container
+       [:nav.navbar.navbar-defaultnavbar-static-top
+        {:role "navigation"}
+        [:div.navbar-header
+         [:button.navbar-toggle
+          {:type "button" :data-toggle "collapse" :data-target "navbar-home-collapse"}
+          [:span.sr-only "Toggle navigation"]
+          [:span.icon-bar]
+          [:span.icon-bar]
+          [:span.icon-bar]
+          ]
+         [:a.navbar-brand {:href "/#"} "neuroses"]
          ]
-        [:a.navbar-brand {:href "/#"} "neuroses"]
+        [:div.collapse.navbar-collapse.navbar-home-collapse
+         [:ul.nav.navbar-nav
+          [:li.active [:a {:href "/"} "home"]]
+          [:li [:a {:href "/about"} "about"]]
+          [:li [:a {:href "/contact"} "contact"]]
+          [:li [:a.text-muted {:href "/#"} [:strike "blog"]]]]]
         ]
-       [:div.collapse.navbar-collapse.navbar-home-collapse
-        [:ul.nav.navbar-nav
-         [:li.active [:a {:href "/"} "home"]]
-         [:li [:a {:href "/about"} "about"]]
-         [:li [:a {:href "/contact"} "contact"]]
-         [:li [:a.text-muted {:href "/#"} [:strike "blog"]]]]]
-       ]
-      (for [frag (:main content-map)] frag)
-      (for [frag (:disqus content-map)] frag)
-      [:div.footer
-       [:blockquote.text-muted "Il semble que la perfection soit atteinte non quand il n'y a plus rien à ajouter, mais quand il n'y a plus rien à retrancher."]
-       [:p.text-muted.pull-right "- Antoine de Saint Exupery"]
-       ]]]))
+       (for [frag (:main content-map)] frag)
+       (for [frag (:disqus content-map)] frag)
+       ]]
+     [:div#footer.container
+      [:blockquote.text-muted "Il semble que la perfection soit atteinte non quand il n'y a plus rien à ajouter, mais quand il n'y a plus rien à retrancher."]
+      [:p.text-muted.pull-right "- Antoine de Saint Exupery"]]]))
 
 (defn index-page [request]
-  (template-base
-   {:main content/index}))
+    (template-base
+     {:main content/index}))
 
 (defn about-page [request]
   (template-base
@@ -68,5 +70,5 @@
    {:main content/contact}))
 
 (defn blog-post [& {:keys [slug timestamp]}]
-  (blog/retrieve-content slug)
-  (template-base))
+  (template-base
+   {:main (blog/retrieve-content :slug slug)}))

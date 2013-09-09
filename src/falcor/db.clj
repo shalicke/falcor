a461609751426ca111c95d98f16744908e53a352
Sam Halicke <sam@halicke.com>
2013-09-09 02:53:54 -0500

    lexical-izing stuff

--- src/falcor/db.clj
+++ src/falcor/db.clj
@@ -6,15 +6,14 @@
             [environ.core :refer [env]]
             [monger.collection :as mc]))
 
-(def db-uri (env :mongolab-uri))
-(def db-name (peek (str/split db-uri #"/")))
-(def default-collection "posts")
 
 (defn initialize!
   "Initializes the database connection."
   []
-  (try
-    (mg/connect-via-uri! db-uri)
-    (mg/set-db! (mg/get-db db-name))
-    (catch Exception e (str "Exception while initializing: " (.getMessage e)))
-    (finally true)))
+  (let [db-uri  (env :mongolab-uri)
+        db-name (peek (str/split db-uri #"/"))]
+    (try
+      (mg/connect-via-uri! db-uri)
+      (mg/set-db! (mg/get-db db-name))
+      (catch Exception e (str "Exception while initializing: " (.getMessage e)))
+      (finally true))))
